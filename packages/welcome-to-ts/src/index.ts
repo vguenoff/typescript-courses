d026fb395715e44431f2fd3a4ac24a4dfc62a3c3
Vasil Genov <vasil.genov@psinteractive.io>
2024-01-30 18:45:48 +0200

    feat: compiling ts

--- packages/welcome-to-ts/src/index.ts
+++ packages/welcome-to-ts/src/index.ts
@@ -3,7 +3,7 @@
  * @param n number of milliseconds before promise resolves
  */
 function timeout(n: number) {
-  return new Promise((res) => setTimeout(res, n));
+  return new Promise((res) => setTimeout(res, n))
 }
 
 /**
@@ -12,18 +12,18 @@ function timeout(n: number) {
  * @param b second
  */
 export async function addNumbers(a: number, b: number) {
-  await timeout(500);
-  return a + b;
+  await timeout(500)
+  return a + b
 }
 
 class Foo {
-  static #bar = 3;
+  static #bar = 3
   static getValue() {
-    return Foo.#bar;
+    return Foo.#bar
   }
 }
 
 //== Run the program ==//
-(async () => {
-  console.log(await addNumbers(Foo.getValue(), 4));
-})();
+;(async () => {
+  console.log(await addNumbers(Foo.getValue(), 4))
+})()
